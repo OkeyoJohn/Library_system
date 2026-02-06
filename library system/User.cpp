4c128cede8cc3b488beab0867f50916806e180d1
OkeyoJohn <136899841+OkeyoJohn@users.noreply.github.com>
2026-02-06 03:11:18 -0800

    Add files via upload
    first commit

---
+++ library system/User.cpp
@@ -0,0 +1,10 @@
+// User.cpp
+#include "User.h"
+
+User::User(int id, string name) {
+    this->userId = id;
+    this->name = name;
+}
+
+int User::getId() { return userId; }
+string User::getName() { return name; }
---
+++ library system/User.h
@@ -0,0 +1,19 @@
+// User.h
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+using namespace std;
+
+class User {
+private:
+    int userId;
+    string name;
+
+public:
+    User(int id, string name);
+    int getId();
+    string getName();
+};
+
+#endif
